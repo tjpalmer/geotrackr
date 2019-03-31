59d915f09819cab4d117ad6737bfd311468ed9ee
Tom <tjpalmer@tjpalmer.com>
2019-03-30 21:00:01 -0700

    Start parsing

--- scripts/src/summarize.ts
+++ scripts/src/summarize.ts
@@ -1,1 +1,6 @@
-console.log('Hi!');
+import {load} from 'cheerio';
+import {readFileSync} from 'fs';
+
+let html = readFileSync('./places/beijing/beijing.html');
+let doc = load(html);
+console.log(doc);
