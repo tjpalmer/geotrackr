4404520dcdd57a4cadff33e23c08a335e6c01617
Tom Palmer <tjpalmer@tjpalmer.com>
2019-04-13 16:15:50 -0700

    Prep for parcel takeover

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('Hello!');
+addEventListener('load', main);
+
+async function main() {
+  // TODO If served from github, grab the commit id and use the explicit rev.
+  let places = await (await fetch('places/places-en.json')).json();
+  console.log(places);
+}
